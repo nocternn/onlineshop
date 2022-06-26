f84ce0352de41e4d441aed2c2fd1f3b23c8f4126
AimsProject <tuanminhlc1206@gmail.com>
2022-06-26 11:55:35 +0700

    Add servlet and JSP but no database and model

--- src/main/java/servlets/ProductInfoServlet.java
+++ src/main/java/servlets/ProductInfoServlet.java
@@ -33,9 +33,8 @@ public class ProductInfoServlet extends HttpServlet {
         
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	// @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
